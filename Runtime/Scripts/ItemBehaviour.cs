c2da225ef6d3d3db9fcba7cd687ad81031462478
IAmBatby <iambatby@gmail.com>
2024-07-22 11:27:26 +1000

    Updated ItemBehaviour

--- Runtime/Scripts/ItemBehaviour.cs
+++ Runtime/Scripts/ItemBehaviour.cs
@@ -6,6 +6,50 @@ namespace IterationToolkit
 {
     public class ItemBehaviour : MonoBehaviour
     {
+        public Rigidbody rigidBody;
         public ItemData itemData;
+
+        public bool IsItemActive {  get; private set; }
+        public bool isItemAvailable { get; private set; }
+
+        public void ToggleItemActive(bool newValue)
+        {
+            IsItemActive = newValue;
+
+            if (IsItemActive == true)
+                OnItemActivated();
+            else
+                OnItemDectivated();
+        }
+
+        public void ToggleItemAvailable(bool newValue)
+        {
+            isItemAvailable = newValue;
+
+            if (isItemAvailable == true)
+                OnItemAvailable();
+            else
+                OnItemUnavailable();
+        }
+
+        protected virtual void OnItemActivated()
+        {
+
+        }
+
+        protected virtual void OnItemDectivated()
+        {
+
+        }
+
+        protected virtual void OnItemAvailable()
+        {
+
+        }
+
+        protected virtual void OnItemUnavailable()
+        {
+
+        }
     }
 }
