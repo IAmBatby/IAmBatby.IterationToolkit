5cf15de9c8e041941d256bcbd8b023421ff138ad
IAmBatby <iambatby@gmail.com>
2024-08-19 15:35:31 +1000

    Added initial networking definition

---
+++ Runtime/NetcodeForGameObjects/NetworkGlobalManager.cs
@@ -0,0 +1,17 @@
+using IterationToolkit;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Netcode;
+using UnityEngine;
+
+public class NetworkGlobalManager : NetworkBehaviour
+{
+    protected static NetworkGlobalManager _manager;
+
+    public ExtendedEvent OnInitalize = new ExtendedEvent();
+
+    protected virtual void Awake()
+    {
+        OnInitalize.Invoke();
+    }
+}
