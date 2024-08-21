bab32ef66b95b00068da899d6d38fa6251376b1f
IAmBatby <iambatby@gmail.com>
2024-08-21 11:53:44 +1000

    Update NetworkGlobalManager.cs

--- Runtime/Netcode/NetworkGlobalManager.cs
+++ Runtime/Netcode/NetworkGlobalManager.cs
@@ -5,6 +5,7 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.Netcode;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace IterationToolkit.Netcode
 {
@@ -13,11 +14,60 @@ namespace IterationToolkit.Netcode
         public static NetworkGlobalManager Instance => Singleton.GetInstance<NetworkGlobalManager>(ref _manager);
         protected static NetworkGlobalManager _manager;
 
+
+        public GameState ActiveGameState { get; private set; }
+
+        public LevelData ActiveLevel { get; private set; }
+        public LevelData LoadingLevel { get; private set; }
+
         public ExtendedEvent OnInitalize = new ExtendedEvent();
+
+        public ExtendedEvent<GameState> OnGameStateChange = new ExtendedEvent<GameState>();
+        public ExtendedEvent<LevelData> OnBeforeLevelLoaded = new ExtendedEvent<LevelData>();
+        public ExtendedEvent<LevelData> OnLevelLoaded = new ExtendedEvent<LevelData>();
 
         protected virtual void Awake()
         {
+            GameObject.DontDestroyOnLoad(gameObject);
             OnInitalize.Invoke();
+        }
+
+
+
+        public void ChangeGameState(GameState newGameState)
+        {
+            ActiveGameState = newGameState;
+
+            OnGameStateChanged();
+
+            OnGameStateChange.Invoke(newGameState);
+        }
+
+        protected virtual void OnGameStateChanged()
+        {
+
+        }
+
+        //Gotta refactor w/ networking in mind (cbf rn)
+        /*
+        public void LoadNewLevel(LevelData newLevelData)
+        {
+            LoadingLevel = newLevelData;
+            OnBeforeLevelLoaded.Invoke(LoadingLevel);
+            SceneManager.sceneLoaded += OnNewLevelLoaded;
+            SceneManager.LoadScene(newLevelData.SceneName);
+        }
+        */
+
+        private void OnNewLevelLoaded(Scene scene, LoadSceneMode mode)
+        {
+            if (scene.name == LoadingLevel.defaultSceneName)
+            {
+                SceneManager.sceneLoaded -= OnNewLevelLoaded;
+                ActiveLevel = LoadingLevel;
+                LoadingLevel = null;
+                OnLevelLoaded.Invoke(ActiveLevel);
+            }
         }
     }
 }
