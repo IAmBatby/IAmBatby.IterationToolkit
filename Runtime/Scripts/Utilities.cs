30b554d06d7f4d1cbd87bdd2966b1fe4a4ddc5ef
IAmBatby <iambatby@gmail.com>
2024-10-29 11:59:00 +1100

    LayerMask Util

--- Runtime/Scripts/Utilities.cs
+++ Runtime/Scripts/Utilities.cs
@@ -1,56 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
 
 namespace IterationToolkit
 {
     public static class Utilities
     {
-        public static int IncreaseIndex(int index, int count)
-        {
-            if (index != count)
-                return (index + 1);
-            else
-                return (0);
-        }
+        public static int IncreaseIndex(int index, int count) => index != count ? index + 1 : 0;
 
-        public static int IncreaseIndex<T>(int index, List<T> collection)
-        {
-            if (index != collection.Count - 1)
-                return (index + 1);
-            else
-                return (0);
-        }
+        public static int IncreaseIndex<T>(int index, List<T> collection) => index != collection.Count - 1 ? index + 1 : 0;
 
-        public static int DecreaseIndex(int index, int count)
-        {
-            if (index != 0)
-                return (index - 1);
-            else
-                return (count);
-        }
+        public static int DecreaseIndex(int index, int count) => index != 0 ? index - 1 : count;
 
-        public static int DecreaseIndex<T>(int index, List<T> collection)
-        {
-            int returnIndex = -1;
+        public static int DecreaseIndex<T>(int index, List<T> collection) => index != 0 ? index = 1 : collection.Count - 1;
 
-            /*
-            if (index != 0)
-                return (index - 1);
-            else if (collection.Count != 0)
-                return (collection.Count - 1);
-            else
-                return (0);
-            */
-            if (index != 0)
-                returnIndex = index - 1;
-            else if (collection.Count != 0)
-                returnIndex = collection.Count - 1;
-            else
-                returnIndex = 0;
-
-            //Debug.Log("Decreasing Index. Index Was: " + index + ", Index Is: " + returnIndex + ", Collection Size Is: " + collection.Count);
-            return returnIndex;
-        }
+        public static bool IsInLayerMask(GameObject obj, LayerMask mask) => (mask.value & (1 << obj.layer)) != 0;
+        public static bool IsInLayerMask(int layer, LayerMask mask) => (mask.value & (1 << layer)) != 0;
     }
 }
