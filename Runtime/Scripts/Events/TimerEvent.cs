a88e6e1a6a1266532c4cb0771e17df617f7b3183
IAmBatby <iambatby@gmail.com>
2024-11-19 16:33:16 +1100

    Added TimerEvent

---
+++ Runtime/Scripts/Events/TimerEvent.cs
@@ -0,0 +1,34 @@
+using IterationToolkit;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TimerEvent : MonoBehaviour
+{
+    public UnityEvent OnTimerStart;
+    public UnityEvent OnTimerFinish;
+    public UnityEvent OnTimerClear;
+
+    [SerializeField] public float defaultTime;
+    private Timer timer;
+
+    public void StartTimerEvent(float time = 0f)
+    {
+        if (time == 0f)
+            time = defaultTime;
+
+
+        if (timer != null)
+        {
+            timer.TryStopTimer();
+            timer = null;
+            OnTimerClear.Invoke();
+        }
+
+        timer = new Timer();
+        timer.onTimerStart.AddListener(OnTimerStart.Invoke);
+        timer.onTimerEnd.AddListener(OnTimerFinish.Invoke);
+        timer.StartTimer(this, time);
+    }
+}
