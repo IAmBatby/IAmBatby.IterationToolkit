191531c4e52cf934026c6f219c9a71c351c03633
IAmBatby <iambatby@gmail.com>
2024-11-19 11:05:02 +1100

    Added Rotator

---
+++ Runtime/Scripts/Rotator.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    [SerializeField] private LayerMask groundMask;
+
+    [SerializeField] private Vector3 groundHitPosition;
+
+    private Vector3 GroundHitMin => groundHitPosition + new Vector3(0, verticalLerpMinOffset, 0);
+    private Vector3 GroundHitMax => GroundHitMin + new Vector3(0, verticalLerpMaxOffset, 0);
+
+    [SerializeField] private float verticalLerpMinOffset;
+    [SerializeField] private float verticalLerpMaxOffset;
+    [SerializeField] private float verticalLerpSpeed;
+
+    [SerializeField] private float horizontalLerpAmount;
+    [SerializeField] private float horizontalLerpSpeed;
+
+    [SerializeField] private bool isRaising;
+
+    private void Awake()
+    {
+        if (Physics.Raycast(transform.position, -transform.up, out RaycastHit hit, Mathf.Infinity, groundMask))
+            groundHitPosition = hit.point;
+        transform.position = Vector3.Lerp(GroundHitMin, GroundHitMax, 0.5f);
+    }
+
+    //Becuase i'm a freak who sometimes wants to call this from other classes
+    public void UpdateRotation()
+    {
+        Vector3 target = isRaising == true ? GroundHitMin : GroundHitMax;
+        transform.position = Vector3.Lerp(transform.position, target, verticalLerpSpeed * Time.deltaTime);
+
+        if (Vector3.Distance(transform.position, target) < 0.1f)
+            isRaising = !isRaising;
+
+        transform.Rotate(new Vector3(0, horizontalLerpAmount, 0));
+    }
+
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawWireCube(groundHitPosition, new Vector3(0.5f, 0.05f, 0.5f));
+        Gizmos.DrawWireCube(GroundHitMin, new Vector3(0.1f, 0.1f, 0.1f));
+        Gizmos.DrawWireCube(GroundHitMax, new Vector3(0.1f, 0.1f, 0.1f));
+        Gizmos.DrawLine(GroundHitMin, GroundHitMax);
+    }
+}
