0289c9206711b4964c6b5f52a272bc1a0604ab16
IAmBatby <iambatby@gmail.com>
2024-12-19 13:46:51 +1100

    Update ExtendedEventManager.cs

--- Runtime/Scripts/ExtendedEventManager.cs
+++ Runtime/Scripts/ExtendedEventManager.cs
@@ -23,6 +23,7 @@ public static class ExtendedEventManager
             }
         }
         Debug.Log("Cleared #" + clearCount + " Static ExtendedEvents.");
+        registeredEvents.Clear();
     }
 
     public static void RegisterExtendedEvent(ExtendedEvent extendedEvent)
