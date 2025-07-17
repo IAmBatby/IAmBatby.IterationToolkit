672d9f063b44b7869a69f6ba00bfce2e5cf9d512
IAmBatby <iambatby@gmail.com>
2025-07-17 10:11:57 +1000

    Functional Refactor?

--- Runtime/Scripts/ExtendedEventManager.cs
+++ Runtime/Scripts/ExtendedEventManager.cs
@@ -1,67 +1,39 @@
 using IterationToolkit;
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
+using System.Reflection;
+
+
+
 
 #if UNITY_EDITOR
 using UnityEditor;
+[InitializeOnLoad]
 #endif
-
 public static class ExtendedEventManager
 {
-    private static List<ExtendedEvent> registeredEvents = new List<ExtendedEvent>();
+    private static Action onRefresh;
 
-    private static List<ExtendedEvent> cachedEvents = new List<ExtendedEvent>();
+    static ExtendedEventManager() => LoadStaticEvents();
 
-    public static void AddEventListener(ExtendedEvent extendedEvent)
+    private static void LoadStaticEvents()
     {
-        if (!registeredEvents.Contains(extendedEvent))
-            registeredEvents.Add(extendedEvent);
-    }
-    [InitializeOnLoadMethod]
-    private static void InitializeOnLoad()
-    {
-        EditorApplication.playModeStateChanged -= OnEditorStateChange;
-        EditorApplication.playModeStateChanged += OnEditorStateChange;
-    }
-    private static void OnEditorStateChange(PlayModeStateChange change)
-    {
-        if (change == PlayModeStateChange.ExitingEditMode) OnEditorEnteringPlay();
-        else if (change == PlayModeStateChange.ExitingPlayMode) OnEditorExitingPlay();
+        onRefresh = null;
+        foreach (MemberInfo member in AppDomain.CurrentDomain.GetAssemblies().SelectMany(a => a.GetTypes()).SelectMany(t => t.GetMembers()))
+            if (member is FieldInfo field && field.IsStatic && field.FieldType == typeof(ExtendedEvent))
+                RegisterStaticEvent(field.GetValue(null) as ExtendedEvent);
     }
 
-    private static void OnEditorExitingPlay()
+    private static void RegisterStaticEvent(ExtendedEvent extendedEvent)
     {
-        //Debug.Log("On Editor Exiting!");
-        registeredEvents = new List<ExtendedEvent>(cachedEvents);
+        if (extendedEvent != null)
+            onRefresh += () => extendedEvent.ClearListeners();
+        else
+            Debug.LogError("Static ExtendedEvent Found Via Reflection Is Null!");
     }
 
-    private static void OnEditorEnteringPlay()
-    {
-        //Debug.Log("On Editor Playing!");
-        cachedEvents = new List<ExtendedEvent>(registeredEvents);
-    }
 
     [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.SubsystemRegistration)]
-    private static void InvokeClear()
-    {
-        List<ExtendedEvent> cache = new List<ExtendedEvent>(registeredEvents);
-        //registeredEvents.Clear();
-        int count = 0;  
-        foreach (ExtendedEvent extendedEvent in cache)
-        {
-            if (extendedEvent.Listeners > 0)
-            {
-                extendedEvent.ClearListeners();
-                count++;
-            }
-
-            //if (!Application.isPlaying)
-            //AddEventListener(extendedEvent);
-        }
-
-        //Debug.Log("Currently Tracking #" + registeredEvents.Count + " Events. Cleared Listeners On #" + count + " Events.");
-    }
-
+    private static void InvokeClear() => onRefresh?.Invoke();
 }
