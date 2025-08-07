aa3914044a2c80eb72f24df5cd52137076d64044
IAmBatby <iambatby@gmail.com>
2025-08-07 18:03:56 +1000

    Update NetworkPlayerBase.cs

--- Runtime/Netcode/NetworkPlayerBase.cs
+++ Runtime/Netcode/NetworkPlayerBase.cs
@@ -10,19 +10,11 @@ namespace IterationToolkit.Netcode
 {
     public class NetworkPlayerBase : NetworkBehaviour
     {
-        public int ClientId => (int)_clientId.Value;
-        [SerializeField] private NetworkVariable<ulong> _clientId = new NetworkVariable<ulong>();
         public NetworkPlayerBase LocalPlayer => GetLocalPlayer<NetworkPlayerBase>();
 
         public T GetLocalPlayer<T>() where T : NetworkPlayerBase
         {
             return GameNetworkManager.Instance.LocalPlayer as T;
-        }
-
-        public override void OnNetworkSpawn()
-        {
-            if (IsServer) _clientId.Value = NetworkManager.ConnectedClients.Values.Where(c => c.PlayerObject == this).FirstOrDefault().ClientId;
-            base.OnNetworkSpawn();
         }
     }
 }
