a9e3ab47e0bcb938a2b0ff14322f99ef39b96648
IAmBatby <iambatby@gmail.com>
2025-08-08 20:17:36 +1000

    Update IListenOnlyEvent.cs

--- Runtime/Scripts/Interfaces/IListenOnlyEvent.cs
+++ Runtime/Scripts/Interfaces/IListenOnlyEvent.cs
@@ -4,16 +4,19 @@ public interface IListenOnlyEvent
 {
     public void AddListener(Action listener);
     public void RemoveListener(Action listener);
+    public void ChangeListener(Action listener, bool value);
 }
 
 public interface IListenOnlyEvent<T> : IListenOnlyEvent
 {
     public void AddListener(Action<T> listener);
     public void RemoveListener(Action<T> listener);
+    public void ChangeListener(Action<T> listener, bool value);
 }
 
 public interface IListenOnlyEvent<T,U> : IListenOnlyEvent
 {
     public void AddListener(Action<T,U> listener);
     public void RemoveListener(Action<T,U> listener);
+    public void ChangeListener(Action<T,U> listener, bool value);
 }
