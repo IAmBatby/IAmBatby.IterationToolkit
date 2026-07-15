b2c6198c31c681f2dff390890300a99792416bf3
IAmBatby <iambatby@gmail.com>
2026-07-15 18:05:33 +1000

    Additional Fixes

--- Runtime/Scripts/Profiler/FPSTracker.cs
+++ Runtime/Scripts/Profiler/FPSTracker.cs
@@ -12,14 +12,17 @@ namespace IterationToolkit
         public static float UpdateFrequency { get; set; } = 0.5f;
 
         private static bool exitEarly = false;
+        private static bool isRunning = false;
 
         [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSplashScreen)]
         private static async void UpdateFPS()
         {
+            if (isRunning) return;
             Debug.Log("Entering UpdateFPS");
             Application.quitting += () => exitEarly = true;
             while (true)
             {
+                isRunning = true;
                 Vector2 prev = new(Time.frameCount, Time.realtimeSinceStartup);
                 await Task.Delay(TimeSpan.FromSeconds(UpdateFrequency));
                 FrameRate = Mathf.RoundToInt((Time.frameCount - prev.x) / (Time.realtimeSinceStartup - prev.y));
@@ -27,6 +30,7 @@ namespace IterationToolkit
                 if (exitEarly)
                 {
                     Debug.Log("Exiting UpdateFPS");
+                    break;
                 }
             }
         }
