c8a3826bc3d34e89d480069acdfa2cdcd5947677
IAmBatby <iambatby@gmail.com>
2026-07-15 17:59:21 +1000

    Swap To Dictionaries

--- Runtime/Scripts/Profiler/ProfilerManager.cs
+++ Runtime/Scripts/Profiler/ProfilerManager.cs
@@ -13,28 +13,42 @@ namespace IterationToolkit
         private static void Start()
         {
             Application.quitting += Clear;
-            activeProfilers.Add(new ProfilerFPSEntry("FPS"));
+            AddEntry(new ProfilerFPSEntry("FPS"));
         }
 
         private static List<IProfilerEntry> activeProfilers = new List<IProfilerEntry>();
 
+        private static Dictionary<string, IProfilerEntry> profileEntries = new Dictionary<string, IProfilerEntry>();
+
         private static void Clear()
         {
-            foreach (IProfilerEntry rec in activeProfilers)
-                rec.Clear();
-            activeProfilers.Clear();
+            foreach (string refName in profileEntries.Keys)
+                RemoveEntry(refName);
         }
 
         public static void RegisterRecorder(ProfilerCategory category, string statName, string displayName)
         {
-            activeProfilers.Add(new ProfilerRecorderEntry(category, statName, displayName));
+            AddEntry(new ProfilerRecorderEntry(category, statName, displayName));
+        }
+
+        private static void AddEntry(IProfilerEntry entry)
+        {
+            if (profileEntries.ContainsKey(entry.ReferenceName)) return;
+            profileEntries[entry.ReferenceName] = entry;
+        }
+
+        private static void RemoveEntry(string referenceName)
+        {
+            if (!profileEntries.ContainsKey(referenceName)) return;
+            profileEntries[referenceName].Clear();
+            profileEntries.Remove(referenceName);
         }
 
         public static string GetProfilerResults()
         {
             string text = "DEBUG VALUES";
 
-            foreach (IProfilerEntry entry in activeProfilers.OrderBy(e => e.CurrentValue))
+            foreach (IProfilerEntry entry in profileEntries.Values.OrderBy(e => e.CurrentValue))
                 text += "\n" + (entry.DisplayName + ": ").Colorize(Color.yellow) + entry.CurrentValue.ToString("N0");
 
             return (text);
